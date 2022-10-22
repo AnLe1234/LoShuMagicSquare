09cc5407688a2d0ab36dc47302bc8e411f9f76ff
An Le <anle1@cpp.edu>
2022-10-22 13:29:18 -0700

    Create initial outline

---
+++ LoShuMagicSquare.c
@@ -0,0 +1,11 @@
+// program that determine a 2D-array is Lo Shu Magic Square
+// create 2D-array
+// create function to test 2D-array
+    // loop compare diagonal
+    // loop compare row
+    // loop compare column
+
+// loop from 1-9 random select 2d array location
+// check if Lo Shu Magic Square
+    // if not count++
+// print count, and 2D array
